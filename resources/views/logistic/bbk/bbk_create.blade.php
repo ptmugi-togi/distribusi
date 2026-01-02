2c3c47248141c81c1474cea389b4ba8ccdf6e1fc
Fr3ezy <calvinjohn810@yahoo.com>
2026-01-02 10:51:20 +0700

    Fixing Formc BBK

--- resources/views/logistic/bbk/bbk_create.blade.php
+++ resources/views/logistic/bbk/bbk_create.blade.php
@@ -34,13 +34,13 @@
           <label for="formc" class="form-label">BBK</label><span class="text-danger"> *</span>
           <select class="form-control select2" id="formc" name="formc" required>
             <option value="" disabled {{ old('formc') ? '' : 'selected' }}>Silahkan Pilih BBK</option>
-            <option value="OF" {{ old('formc') == 'OF' ? 'selected' : '' }}>OF (ISSUE ADJUSTMENT)</option>
-            <option value="OC" {{ old('formc') == 'OC' ? 'selected' : '' }}>OC (ISSUE TO WORKSHOP)</option>
-            <option value="OB" {{ old('formc') == 'OB' ? 'selected' : '' }}>OB (ISSUE TO PRODUCTION)</option>
-            <option value="OD" {{ old('formc') == 'OD' ? 'selected' : '' }}>OD (ISSUE WRITE OFF)</option>
-            <option value="OG" {{ old('formc') == 'OG' ? 'selected' : '' }}>OG (OFFICE USED)</option>
             <option value="OA" {{ old('formc') == 'OA' ? 'selected' : '' }}>OA (ISSUE DIPINJAM/DEMO)</option>
+            <option value="OB" {{ old('formc') == 'OB' ? 'selected' : '' }}>OB (ISSUE TO PRODUCTION)</option>
+            <option value="OC" {{ old('formc') == 'OC' ? 'selected' : '' }}>OC (ISSUE TO WORKSHOP)</option>
+            <option value="OD" {{ old('formc') == 'OD' ? 'selected' : '' }}>OD (ISSUE WRITE OFF)</option>
             <option value="OE" {{ old('formc') == 'OE' ? 'selected' : '' }}>OE (ISSUE WARRANTY CLAIM)</option>
+            <option value="OF" {{ old('formc') == 'OF' ? 'selected' : '' }}>OF (ISSUE ADJUSTMENT)</option>
+            <option value="OG" {{ old('formc') == 'OG' ? 'selected' : '' }}>OG (OFFICE USED)</option>
             {{-- FormC lain nanti --}}
           </select>
         </div>
