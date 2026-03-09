a0570889e38ea78bbbdad30f2a514923cd120dd6
Fr3ezy <calvinjohn810@yahoo.com>
2026-03-09 08:45:08 +0700

    fix temp

--- resources/views/marketing/oc_sb/oc_sb_index.blade.php
+++ resources/views/marketing/oc_sb/oc_sb_index.blade.php
@@ -55,13 +55,13 @@
                                 <span class="badge bg-danger">CANCELED</span>
                               @endif
                               {{-- preview --}}
-                              <a href="{{ route('oc.previewOcSb', $o->ocsbid) }}" class="badge bg-success" data-tooltip="true" data-bs-placement="top" title="Preview"><i class="bi bi-file-earmark-image-fill"></i></a>
+                              {{-- <a href="{{ route('oc.previewOcSb', $o->ocsbid) }}" class="badge bg-success" data-tooltip="true" data-bs-placement="top" title="Preview"><i class="bi bi-file-earmark-image-fill"></i></a> --}}
 
                               {{-- print --}}
                               @if (!$periodClosed && $o->braco == auth()->user()->cabang)
-                                @if ($o->resta != 'C')
+                                {{-- @if ($o->resta != 'C')
                                   <a href="{{ route('oc.printOcSb', $o->ocsbid) }}" class="badge bg-success" data-tooltip="true" data-bs-placement="top" title="Print"><i class="bi bi-file-earmark-arrow-down"></i></a>
-                                @endif
+                                @endif --}}
                                 <a href="/oc-sb/{{ $o->ocsbid }}/detail" class="badge bg-primary" data-tooltip="true" data-bs-placement="top" title="Detail"><i class="bi bi-info-circle"></i></a>
                                 
                                 @if ($o->resta != 'C')
