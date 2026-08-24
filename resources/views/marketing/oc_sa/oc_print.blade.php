80ad231d78806ff518072766d228909f9ab269e9
Fr3ezy <calvinjohn810@yahoo.com>
2026-08-24 09:11:10 +0700

    guard docd in oc print

--- resources/views/marketing/oc_sa/oc_print.blade.php
+++ resources/views/marketing/oc_sa/oc_print.blade.php
@@ -363,8 +363,8 @@
 <htmlpagefooter name="myFooter">
     <hr>
 
-    <div style="font-size: 10px">{{ $ochdr->mformcode->docd1 }}</div>
-    <div style="font-size: 10px">{{ $ochdr->mformcode->docd2 }}</div>
+    <div style="font-size: 10px">{{ $ochdr->mformcode->docd1 ?? '' }}</div>
+    <div style="font-size: 10px">{{ $ochdr->mformcode->docd2 ?? '' }}</div>
     <div style="font-size: 10px">{{ $ochdr->created_by }} / {{ date('d-m-Y') }} / {{ date('H:i:s') }} / {{ $ochdr->prctr }}</div>
     <div style="text-align: right; font-size: 9pt;">
         {PAGENO}/{nbpg}
